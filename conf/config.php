5abfdd9793cbc96fd12d15bd7b94a518326fadb3
Marcel Hauri <hello@m83.ch>
2010-09-21 12:01:55 +0200

    Spelling

--- conf/config.php
+++ conf/config.php
@@ -16,7 +16,7 @@ $CONF['INTERVAL'] = '60';
 // Configure User Agent
 $CONF['USER_AGENT'] = 'SITETEST (ver.: 0.9.8 / http://projects.m83.ch/sitetest/';
 
-// Actice Template
+// Active Template
 $CONF['TEMPLATE'] = 'default.tpl.php';
 
 
@@ -30,7 +30,7 @@ $CONF['CHECK']['drs1.ch'] = array('adress' => 'http://www.drs1.ch/',
 $CONF['CHECK']['drs2.ch'] = array('adress' => 'http://www.drs2.ch/',
                                   'type'   => 'text/html');
 
-$CONF['CHECK']['drs3.ch'] = array('adress' => 'http://www.drs1.ch/',
+$CONF['CHECK']['drs3.ch'] = array('adress' => 'http://www.drs3.ch/',
                                   'type'   => 'text/html');
 
 
